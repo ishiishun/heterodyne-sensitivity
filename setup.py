3e14b03a4fd26aaef974c57fce0c5535e1c96e7a
ishiishun <63507357+ishiishun@users.noreply.github.com>
2022-02-10 00:06:48 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
-#!usr/bin/env python
-
-from setuptools import setup, find_packages
-
-setup(name="LST_heterodyne_sensitivity_package",
-	version="0.1",
-	description="Test for pip install git+",
-	url="https://github.com/ishiishun/heterodyne-sensitivity",
-	packages=find_packages(),
-	)
+from setuptools import setup
+setup()
